26d0b7b1ca140669a88c2385f74952a71700c83a
Yash Kaushik <kaushikyash262@gmail.com>
2025-09-24 23:14:54 +0530

    Added Domain

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000", "https://door-dash-sigma.vercel.app", "https://gullyfoods.app/"],
+    origin: ["http://localhost:3000", "https://door-dash-sigma.vercel.app", "https://gullyfoods.app"],
     methods: ["POST", "PUT", "GET", "DELETE"],
   })
 );
