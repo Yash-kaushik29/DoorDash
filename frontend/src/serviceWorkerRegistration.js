1f6294fd46b73ea2f37843f7c8b98b4ea6fa5494
Yash Kaushik <kaushikyash262@gmail.com>
2025-12-08 23:59:50 +0530

    Minor Fixes

--- frontend/src/serviceWorkerRegistration.js
+++ frontend/src/serviceWorkerRegistration.js
@@ -1,81 +1,9 @@
-// src/serviceWorkerRegistration.js
+import { Workbox } from 'workbox-window';
 
-const isLocalhost = Boolean(
-  window.location.hostname === "localhost" ||
-    window.location.hostname === "[::1]" ||
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
-);
+export function register() {
+  if ('serviceWorker' in navigator) {
+    const wb = new Workbox(`${process.env.PUBLIC_URL}/service-worker.js`);
 
-export function register(config) {
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      const swUrl = "/serviceWorkerRegistration.js";
-
-      if (isLocalhost) {
-        checkValidServiceWorker(swUrl, config);
-        navigator.serviceWorker.ready.then(() => {
-          console.log("Service Worker is ready on localhost.");
-        });
-      } else {
-        registerValidSW(swUrl, config);
-      }
-    });
-  }
-}
-
-function registerValidSW(swUrl, config) {
-  navigator.serviceWorker
-    .register(swUrl)
-    .then((registration) => {
-      registration.onupdatefound = () => {
-        const installingWorker = registration.installing;
-        if (installingWorker == null) return;
-
-        installingWorker.onstatechange = () => {
-          if (installingWorker.state === "installed") {
-            if (navigator.serviceWorker.controller) {
-              console.log("New content available; will be used after closing tabs.");
-
-              if (config && config.onUpdate) config.onUpdate(registration);
-            } else {
-              console.log("Content cached for offline use.");
-              if (config && config.onSuccess) config.onSuccess(registration);
-            }
-          }
-        };
-      };
-    })
-    .catch((error) => {
-      console.error("Error during service worker registration:", error);
-    });
-}
-
-function checkValidServiceWorker(swUrl, config) {
-  fetch(swUrl, { headers: { "Service-Worker": "script" } })
-    .then((response) => {
-      const contentType = response.headers.get("content-type");
-      if (
-        response.status === 404 ||
-        (contentType != null && contentType.indexOf("javascript") === -1)
-      ) {
-        navigator.serviceWorker.ready.then((registration) => {
-          registration.unregister().then(() => window.location.reload());
-        });
-      } else {
-        registerValidSW(swUrl, config);
-      }
-    })
-    .catch(() => {
-      console.log("No internet connection found. Running in offline mode.");
-    });
-}
-
-export function unregister() {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.ready
-      .then((registration) => registration.unregister())
-      .catch((error) => console.error(error.message));
+    wb.register();
   }
 }
