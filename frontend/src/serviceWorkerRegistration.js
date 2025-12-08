e197f16669afc49ffbf10f7bfbf95cd06f57efcd
Yash Kaushik <kaushikyash262@gmail.com>
2025-12-09 00:12:42 +0530

    Minor Fixes

--- frontend/src/serviceWorkerRegistration.js
+++ frontend/src/serviceWorkerRegistration.js
@@ -2,7 +2,7 @@ import { Workbox } from 'workbox-window';
 
 export function register() {
   if ('serviceWorker' in navigator) {
-    const wb = new Workbox(`${process.env.PUBLIC_URL}/service-worker.js`);
+    const wb = new Workbox(`${process.env.PUBLIC_URL || ''}/service-worker.js`);
 
     wb.register();
   }
