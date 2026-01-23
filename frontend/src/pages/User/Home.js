50f93bb655732b294c3823548f30eb863dd1b2cf
Yash Kaushik <kaushikyash262@gmail.com>
2026-01-24 00:18:10 +0530

    UI Fixes

--- frontend/src/pages/User/Home.js
+++ frontend/src/pages/User/Home.js
@@ -290,7 +290,7 @@ const Home = () => {
       {/* PRODUCTS */}
       <section className="py-12 container mx-auto px-4">
         <h2 className="text-3xl font-bold text-center mb-8">
-          Our Picks 🎅
+          ✨ Our Picks ✨
         </h2>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
           {memoizedProducts}
